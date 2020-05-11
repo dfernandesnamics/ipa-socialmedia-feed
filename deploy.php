0225308832342e23c8a097e6fb937a6ae241d009
Daniel Araújo Fernandes <daniel.fernandes@namics.com>
2020-05-11 11:11:46 +0200

    added git deployment

---
+++ deploy.php
@@ -0,0 +1,105 @@
+{\rtf1\ansi\ansicpg1252\cocoartf1671\cocoasubrtf600
+{\fonttbl\f0\fmodern\fcharset0 Courier;}
+{\colortbl;\red255\green255\blue255;\red0\green0\blue0;}
+{\*\expandedcolortbl;;\cssrgb\c0\c0\c0;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww10800\viewh8400\viewkind0
+\deftab720
+\pard\pardeftab720\sl280\partightenfactor0
+
+\f0\fs24 \cf2 \expnd0\expndtw0\kerning0
+<?php\
+\
+// Forked from https://gist.github.com/1809044\
+// Available from https://gist.github.com/nichtich/5290675#file-deploy-php\
+\
+$TITLE   = 'Git Deployment Hamster';\
+$VERSION = '0.11';\
+\
+echo <<<EOT\
+<!DOCTYPE HTML>\
+<html lang="en-US">\
+<head>\
+	<meta charset="UTF-8">\
+	<title>$TITLE</title>\
+</head>\
+<body style="background-color: #000000; color: #FFFFFF; font-weight: bold; padding: 0 10px;">\
+<pre>\
+  o-o    $TITLE\
+ /\\\\"/\\   v$VERSION\
+(`=*=') \
+ ^---^`-.\
+\
+\
+EOT;\
+\
+// Check whether client is allowed to trigger an update\
+\
+$allowed_ips = array(\
+	'207.97.227.', '50.57.128.', '108.171.174.', '50.57.231.', '204.232.175.', '192.30.252.', // GitHub\
+	'195.37.139.','193.174.' // VZG\
+);\
+$allowed = false;\
+\
+$headers = apache_request_headers();\
+\
+if (@$headers["X-Forwarded-For"]) \{\
+    $ips = explode(",",$headers["X-Forwarded-For"]);\
+    $ip  = $ips[0];\
+\} else \{\
+    $ip = $_SERVER['REMOTE_ADDR'];\
+\}\
+\
+foreach ($allowed_ips as $allow) \{\
+    if (stripos($ip, $allow) !== false) \{\
+        $allowed = true;\
+        break;\
+    \}\
+\}\
+\
+if (!$allowed) \{\
+	header('HTTP/1.1 403 Forbidden');\
+ 	echo "<span style=\\"color: #ff0000\\">Sorry, no hamster - better convince your parents!</span>\\n";\
+    echo "</pre>\\n</body>\\n</html>";\
+    exit;\
+\}\
+\
+flush();\
+\
+// Actually run the update\
+\
+$commands = array(\
+	'echo $PWD',\
+	'whoami',\
+	'git pull',\
+	'git status',\
+	'git submodule sync',\
+	'git submodule update',\
+	'git submodule status',\
+    'test -e /usr/share/update-notifier/notify-reboot-required && echo "system restart required"',\
+);\
+\
+$output = "\\n";\
+\
+$log = "####### ".date('Y-m-d H:i:s'). " #######\\n";\
+\
+foreach($commands AS $command)\{\
+    // Run it\
+    $tmp = shell_exec("$command 2>&1");\
+    // Output\
+    $output .= "<span style=\\"color: #6BE234;\\">\\$</span> <span style=\\"color: #729FCF;\\">\{$command\}\\n</span>";\
+    $output .= htmlentities(trim($tmp)) . "\\n";\
+\
+    $log  .= "\\$ $command\\n".trim($tmp)."\\n";\
+\}\
+\
+$log .= "\\n";\
+\
+file_put_contents ('deploy-log.txt',$log,FILE_APPEND);\
+\
+echo $output; \
+\
+?>\
+</pre>\
+</body>\
+</html>\
+}
