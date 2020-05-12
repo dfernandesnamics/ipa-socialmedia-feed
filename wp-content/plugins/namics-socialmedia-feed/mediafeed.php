9ffc4623a298bda596b69933ab038af348fd2879
Daniel Araújo Fernandes <daniel.fernandes@namics.com>
2020-05-12 11:24:48 +0200

    Add Tedst File Upload

--- wp-content/plugins/namics-socialmedia-feed/mediafeed.php
+++ wp-content/plugins/namics-socialmedia-feed/mediafeed.php
@@ -7,38 +7,57 @@
 */
 /* Plugin initialisieren, Wordpress erkennt das Plugin durch den Kommentarblock. */
 
-function form_creation() {
+function form_creation()
+{
     ?>
     <form class="form-submit-post" method="post">
         <label>Caption:
             <input class="input-caption" type="text" name="post-caption" required>
         </label>
         <label>Image:
-            <?php media_upload_form() ?>
+            <input type='file' name='post-image'>
         </label>
         <input class="input-submit-post" type="submit" value="Post" name="submit-post">
     </form> <?php
 }
-add_action( 'template_redirect', 'createPost' );
-function createPost() {
-    if ( isset( $_POST['submit-post'] ) ) {
+
+add_action('template_redirect', 'createPost');
+
+function createPost()
+{
+    if (isset($_POST['submit-post'])) {
+
+        if ($_FILES['file']['name'] != '') {
+            $uploadedFile = $_FILES['file'];
+            $upload_overrides = array('test_form' => false);
+
+            $movefile = wp_handle_upload($uploadedFile, $upload_overrides);
+            $imageurl = "";
+            if ($movefile && !isset($movefile['error'])) {
+                $imageurl = $movefile['url'];
+                echo "url : " . $imageurl;
+            } else {
+                echo $movefile['error'];
+            }
+        }
+
         $the_post = array(
-            'post_title'    => wp_strip_all_tags( $_POST['post-caption'] ),
-            'post_image'    => wp_strip_all_tags( $_POST['post-image'] ),
-            'post_status'   => 'publish',
-            'post_author'   => get_current_user_id(),
-            'post_type'     => 'post',
+            'post_title' => wp_strip_all_tags($_POST['post-caption']),
+            'post_image' => wp_strip_all_tags($_POST['post-image']),
+            'post_status' => 'publish',
+            'post_author' => get_current_user_id(),
+            'post_type' => 'post',
             'post_category' => 0
         );
-        sanitize_post( $the_post );
-        wp_insert_post( $the_post );
+        sanitize_post($the_post);
+        wp_insert_post($the_post);
 
-        wp_redirect( "/" );
+        wp_redirect("/");
         die();
     }
 
 }
 
-add_shortcode( 'user-posts', 'form_creation' );
+add_shortcode('user-posts', 'form_creation');
 ?>
 
